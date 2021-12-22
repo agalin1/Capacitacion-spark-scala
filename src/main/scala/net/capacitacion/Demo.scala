603ff0831a2654d1d939acf62ff08c14fe4e8c92
andres galindo <agalinvar9@gmail.com>
2021-12-22 10:47:52 -0500

    prueba dataframe

--- src/main/scala/net/capacitacion/Demo.scala
+++ src/main/scala/net/capacitacion/Demo.scala
@@ -5,9 +5,37 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Demo {
 
-    def main(args: Array[String]): Unit = {
+  var nameApp = "CapacitacionSpark"
+  var pathInput = "input/MOCK_DATA_1.csv"
+  var delimitador = "|"
+  var sc: SparkContext = null
 
-    println("Hola mundo")
+  val spark: SparkSession = SparkSession.builder
+    .appName(nameApp)
+    .master("local[*]")
+    .getOrCreate()
+
+  println("Hola mundo")
+
+  var dfInputData: DataFrame = spark.emptyDataFrame
+
+
+  def main(args: Array[String]): Unit = {
+
+    sc = spark.sparkContext
+
+    dfInputData = spark.read
+      .option("delimiter", delimitador)
+      .option("header", "true")
+      .option("charset", "UTF-8")
+      .option("inferSchema", "true")
+      .option("nullValue", "null")
+      .csv(pathInput)
+
+    dfInputData.show(false)
+    dfInputData.printSchema()
+
+    println("Se realizaron pruebas unitarias")
 
 
   }
