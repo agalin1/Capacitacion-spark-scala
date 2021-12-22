d9556b5eb225c9f7a205a1f1c8a942aee2138af3
andres galindo <agalinvar9@gmail.com>
2021-12-22 10:34:27 -0500

    first hola mundo

--- src/main/scala/net/capacitacion/Demo.scala
+++ src/main/scala/net/capacitacion/Demo.scala
@@ -1,7 +1,16 @@
 package net.capacitacion
 
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
 object Demo {
 
+    def main(args: Array[String]): Unit = {
+
+    println("Hola mundo")
+
+
+  }
 
 
 }
